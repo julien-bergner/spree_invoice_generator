100ccba5098fadf55f63d48cabc1faa7227343b2
Xiaojun Dai <dai.xiaojun@gmail.com>
2013-12-04 23:30:45 +1300

    remove pdf invoice from frontend

--- app/overrides/add_button_to_customer_panel.rb
+++ app/overrides/add_button_to_customer_panel.rb
@@ -7,4 +7,4 @@ Deface::Override.new(:virtual_path => %q{spree/users/show},
 Deface::Override.new(:virtual_path => %q{spree/users/show},
                      :insert_after => "table.order-summary tbody tr td:last",
                       :name => "invoice_print_user_link",
-                      :text => %q{<td><%= link_to t(:show, :scope => :spree), pdf_invoice_path(:order_id => order.id, :format => :html), :onclick => "window.open(this.href, '#{t(:invoice, :scope => :spree)}', 'width=745,height=892,left=100,top=100,menubar=no,toolbar=yes,scrollbars=yes,location=no,hotkeys=yes'); return false;" %><%= link_to t(:download, :scope => :spree), pdf_invoice_path(:order_id => order.id, :format => :pdf) %></td>})
+                      :text => %q{<td><%= link_to t(:show, :scope => :spree), pdf_invoice_path(:order_id => order.id, :format => :html), :onclick => "window.open(this.href, '#{t(:invoice, :scope => :spree)}', 'width=745,height=892,left=100,top=100,menubar=no,toolbar=yes,scrollbars=yes,location=no,hotkeys=yes'); return false;" %></td>})
