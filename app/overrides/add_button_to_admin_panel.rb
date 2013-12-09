d218a405d7d4ef7325051a8662b03cc4bc77dcc2
Xiaojun Dai <dai.xiaojun@gmail.com>
2013-12-09 21:38:34 +1300

    remove pdf icon

--- app/overrides/add_button_to_admin_panel.rb
+++ app/overrides/add_button_to_admin_panel.rb
@@ -7,4 +7,4 @@ Deface::Override.new(:virtual_path => %q{spree/admin/orders/index},
 Deface::Override.new(:virtual_path => %q{spree/admin/orders/index},
                      :insert_after => "[data-hook='admin_orders_index_row_actions']",
                      :name => "invoice_print_link",
-                     :text => %q{<td><%= link_to "#{image_tag('admin/icons/receipt.png')} #{t(:show, :scope => :spree)}".html_safe, pdf_invoice_path(:order_id => order.id, :format => :html), :onclick => "window.open(this.href, '#{t(:invoice, :scope => :spree)}', 'width=745,height=892,left=100,top=100,menubar=no,toolbar=yes,scrollbars=yes,location=no,hotkeys=yes'); return false;" %>&nbsp;<%= link_to "#{image_tag('admin/icons/pdf.png')} #{t(:download, :scope => :spree)}".html_safe, pdf_invoice_path(:order_id => order.id, :format => :pdf) %></td>})
+                     :text => %q{<td><%= link_to "#{image_tag('admin/icons/receipt.png')} #{t(:show, :scope => :spree)}".html_safe, :format => :html), :onclick => "window.open(this.href, '#{t(:invoice, :scope => :spree)}', 'width=745,height=892,left=100,top=100,menubar=no,toolbar=yes,scrollbars=yes,location=no,hotkeys=yes'); return false;" %></td>})
