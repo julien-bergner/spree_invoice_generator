aba5947119dfaf7fbb76df69f4e40d59c0fc480e
Julien Bergner <julien@bergner.fr>
2014-01-23 00:07:15 +0100

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,3 @@
 Spree::Core::Engine.routes.draw do
-  match '/invoice/show/:order_id' => 'invoice#show', :as => :pdf_invoice
+  match '/invoice_prints/show/:order_id' => 'invoice_prints#show', :as => :pdf_invoice_prints, via: [:get, :post]
 end
