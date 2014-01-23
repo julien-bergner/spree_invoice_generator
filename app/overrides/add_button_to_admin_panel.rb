0325d6f1b7e6326d8119ee9d0ba964136d7583fa
Julien Bergner <julien@bergner.fr>
2014-01-23 02:47:12 +0100

    Update add_button_to_admin_panel.rb

--- app/overrides/add_button_to_admin_panel.rb
+++ app/overrides/add_button_to_admin_panel.rb
@@ -1,13 +1,13 @@
 # Admin Panel buttons
 Deface::Override.new(:virtual_path => %q{spree/admin/orders/index},
-                     :insert_after => "[data-hook='admin_orders_index_header_actions']",
+                     :insert_before => "[data-hook='admin_orders_index_header_actions']",
                      :name => "invoice_print_header",
                      :text => "<th><%= t(:invoice, :scope => :spree) %></th>")
 
 Deface::Override.new(:virtual_path => %q{spree/admin/orders/index},
-                     :insert_after => "[data-hook='admin_orders_index_row_actions']",
+                     :insert_before => "[data-hook='admin_orders_index_row_actions']",
                      :name => "invoice_print_link",
-                     :text => %q{<td><%= link_to "#{image_tag('admin/icons/receipt.png')} #{t(:show, :scope => :spree)}".html_safe, pdf_invoice_path(:order_id => order.id, :format => :html), :onclick => "window.open(this.href, '#{t(:invoice, :scope => :spree)}', 'width=745,height=892,left=100,top=100,menubar=no,toolbar=yes,scrollbars=yes,location=no,hotkeys=yes'); return false;" %></td>})
+                     :text => %q{<td><%= link_to "#{image_tag('admin/icons/receipt.png')} #{t(:show, :scope => :spree)}".html_safe, pdf_invoice_path(:order_id => order.id, :format => :pdf), :onclick => "window.open(this.href, '#{t(:invoice, :scope => :spree)}', 'width=745,height=892,left=100,top=100,menubar=no,toolbar=yes,scrollbars=yes,location=no,hotkeys=yes'); return false;" %></td>})
 
 Deface::Override.new(:virtual_path => %q{spree/admin/orders/show},
                      :insert_bottom => "[data-hook='admin_order_show_buttons']",
